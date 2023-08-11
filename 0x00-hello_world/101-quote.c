4dc30687939d37be9bcf44763b48ed4e2d00178c
amrabdelghany <joo_amr91@yahoo.com>
2023-08-10 19:41:54 -0700

    101 done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: This project will return 1
+ *
+ *Return: 1 
+*/
+
+int main(void)
+{
+print("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
