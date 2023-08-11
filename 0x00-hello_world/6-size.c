3f32326b5a75b9feb94449eb8799270c580afcb2
amrabdelghany <joo_amr91@yahoo.com>
2023-08-10 19:26:07 -0700

    added the missing par

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,14 @@
  *
  *Description: This program for mesure the sizeof various inputs on computer"
  *
- *Return:  Always 0 (Success)
+ *Return: Always 0 (Success)
 */
+
 int main(void)
 {
 printf("Size of a char: %u byte(s)\n", sizeof(char));
 printf("Size of an int: %u byte(s)\n", sizeof(int));
-printf("Size of a long int: %u byte(s)\n", sizeof(long));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 printf("Size of along long int: %u byte(s)\n", sizeof(long long int));
 printf("Size of a float: %u byte(s)\n", sizeof(float));
 return (0);
