ebeab7af54d9b39d417ed5b60a710e4fc373c1d1
amrabdelghany <joo_amr91@yahoo.com>
2023-08-10 19:57:34 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  *main - Entry point
  *
- *Description: This project will return 1
+ *Description: This project will print a quite using write function
+ *          write(int fd.const void *buf,size_t count)
  *
- *Return:1(Success)
+ *Return: 1 (Not Success)
 */
 int main(void)
 {
-print("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char art[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(1,art,59);
 return (1);
 }
