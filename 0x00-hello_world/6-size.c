17f642b200c185be2e1f288100c0ec0469628c53
amrabdelghany <joo_amr91@yahoo.com>
2023-08-10 19:03:50 -0700

    fixed the return value

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,11 @@
  *
  *Description: This program for mesure the sizeof various inputs on computer"
  *
- *Return" always 0 (Sucess)
+ *Return:  always 0 (Sucess)
 */
 int main(void)
 {
-printf("size of a: %lu byte(s)\n", sizeof(char));
+printf("size of a char: %lu byte(s)\n", sizeof(char));
 printf("size of an int: %lu byte(s)\n", sizeof(int));
 printf("size of a long int: %lu byte(s)\n", sizeof(long));
 printf("size of along long int: %lu byte(s)\n", sizeof(long long int));
