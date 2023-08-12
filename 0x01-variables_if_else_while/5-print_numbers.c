71f6e1c9ef49d7594aef0746c6cb9db3a4f3ff71
amrabdelghany <joo_amr91@yahoo.com>
2023-08-11 19:33:19 -0700

    file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poin
+ *
+ * Description: print the alphabit exept q and e
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf('\n');
+	return (0);
+}
+
