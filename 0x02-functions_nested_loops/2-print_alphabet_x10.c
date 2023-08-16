f1cb8fd0215fde3dc15a9c367f23bc52cb0797fe
amrabdelghany <joo_amr91@yahoo.com>
2023-08-15 18:06:27 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - utlizzes _putchar and nested loop to print
+ *		a - z in lower case
+*/
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int ch = 10;
+
+	while (ch < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	}
+	_putchar('\n');
+}
