8d35daf7c87461807c9e0d62f8567aa6ac78c60e
amrabdelghany <joo_amr91@yahoo.com>
2023-08-15 17:19:36 -0700

    added ignore rule for _putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints from a to z using putchar prototype
+ *
+ * Return: 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; a <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
