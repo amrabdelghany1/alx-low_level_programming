93522de29156d0dc77384ac9e46da2779451759f
amrabdelghany <joo_amr91@yahoo.com>
2023-08-15 18:09:56 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@
 void print_alphabet_x10(void)
 {
 	char letter;
-	int ch = 10;
+	int ch = 0;
 
 	while (ch < 10)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-	}
 	_putchar('\n');
+	}
 }
