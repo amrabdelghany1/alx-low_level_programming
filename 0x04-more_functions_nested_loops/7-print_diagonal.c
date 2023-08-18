22295eec10943ad23c3b40f31a9b12715c8bd280
amrabdelghany <joo_amr91@yahoo.com>
2023-08-18 16:25:17 -0700

    done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints \ if the
+ *		input is greater than 0
+ *
+ * @n: to check the function input	
+*/
+
+void print_diagonal(int n)
+{
+	int line;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+		for (line = 0; line < n; line++)
+		{
+			_putchar('\\');
+                	_putchar('\n');
+		}
+}
