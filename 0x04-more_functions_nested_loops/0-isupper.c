2a779517c38ae24fc3f93be7e67e38c01e79eab8
amrabdelghany <joo_amr91@yahoo.com>
2023-08-17 18:36:10 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check if the letter is Upper case
+ *
+ * @c: check function input
+ *
+ * Return: 0 (success)
+*/
+
+int _isupper(int c)
+{
+	int ch;
+
+	if (ch >= 65 && ch <= 90)
+		putchar(ch);
+	return (0);
+}
