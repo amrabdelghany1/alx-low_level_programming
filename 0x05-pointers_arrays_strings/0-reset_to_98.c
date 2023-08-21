ec2da37378d5a229ad16f8ecbb70e61e8d094cf4
amrabdelghany <joo_amr91@yahoo.com>
2023-08-21 16:49:13 -0700

    done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that create a pointer of n and return it 
+ *		to 98
+ *
+ * @*n: checks the function input
+*/
+
+void reset_to_98(int *n)
+{
+	int *ptr = &n;
+	*ptr = 98;
+}
