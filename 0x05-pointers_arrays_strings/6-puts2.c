d950248af5ab5512cf6f49c781740c24773995ac
amrabdelghany <joo_amr91@yahoo.com>
2023-08-21 18:47:39 -0700

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; *str != '\0'; str + 2)
-		_putchar(s[i]);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 
 }
