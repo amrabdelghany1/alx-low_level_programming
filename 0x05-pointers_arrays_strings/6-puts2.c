22bc04d411345cdb3b2c5044cba04e49bb5d29a1
amrabdelghany <joo_amr91@yahoo.com>
2023-08-21 18:43:57 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function that print every other
+ *		char of a string
+ *
+ * @str: check the function input
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *srt != '\0'; str + 2)
+		_putchar(s[i]);
+	_putchar('\n');
+
+}
