bca139056826fe9b3e62adac5812e10026c4dd66
amrabdelghany <joo_amr91@yahoo.com>
2023-08-25 18:38:10 -0700

    done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ *
+ * @str: check the input function
+ *
+ * Return: ptr
+*/
+
+char *cap_string(char *str)
+{
+	int i = 1;
+	char *ptr = str;
+
+	while (*str)
+	{
+		if (isDelimiter(*s))
+		{
+			i = 1;
+		}
+		else if
+		{
+			*str -= 32;
+			i = 0;
+		}
+		else
+			i = 0;
+		str++;
+	}
+	return (ptr);
+}
