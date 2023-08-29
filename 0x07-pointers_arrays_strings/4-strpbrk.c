45afc44b3b90488f225443f46cabca0a53ef0014
amrabdelghany <joo_amr91@yahoo.com>
2023-08-28 19:36:31 -0700

    done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,20 +7,25 @@
  * @s: check the input function
  * @accept: checks the input func
  *
- * Return: i
+ * Return: p if there is matching
+ *		otherwise 0
 */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+	char *p;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
-				return (i);
+			{
+				p = &s[i];
+				return (p);
+			}
 		}
 	}
-	return (i);
+	return (0);
 }
