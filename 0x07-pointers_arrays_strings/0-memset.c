c274de341d9b641055cd635ae6a25071a4154372
amrabdelghany <joo_amr91@yahoo.com>
2023-08-28 18:25:07 -0700

    done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - func that check the n numbers of
+ *		b signs and exchange them on
+ *		the first n numbers in s
+ *
+ * @s: checks the function input
+ * @b: checks the function input
+ * @n: checks the int input
+ *
+ * Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unasigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
