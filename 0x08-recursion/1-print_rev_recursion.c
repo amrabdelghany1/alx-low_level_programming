e11b6f9832a0a00b10c16d8c0920a9658ea2b648
amrabdelghany <joo_amr91@yahoo.com>
2023-08-30 17:52:15 -0700

    done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _ptint_rev_recursion - func that print a reversed
+ *		string
+ *
+ * @s: input
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
