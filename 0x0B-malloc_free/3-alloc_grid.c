043d7111f5044bbdbc2ccc91aa49accbe580523a
amrabdelghany <joo_amr91@yahoo.com>
2023-09-06 19:06:35 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,6 @@ int **alloc_grid(int width, int height)
 	int **tab, i, j;
 
 	tab = malloc(sizeof(*tab) * height);
-
 	if (width <= 0 || height <= || tab == 0)
 	{
 		return (NULL);
@@ -32,7 +31,7 @@ int **alloc_grid(int width, int height)
 				free(tab);
 				return (NULL);
 			}
-			for (i = 0; j < width; j++)
+			for (j = 0; j < width; j++)
 				tab[i][j] = 0;
 		}
 	}
