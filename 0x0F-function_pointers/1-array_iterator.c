d49c3d827087498527061a10bebd16d08ce90b63
amrabdelghany <joo_amr91@yahoo.com>
2023-09-15 16:07:08 -0700

    done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate throw an array
+ *		throw a func pointer
+ *
+ * @array: input array
+ * @size: input size
+ * @action: func pointer
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int *end = array + size - 1;
+
+	if (array && size && actoin)
+		while (array <= end)
+			action(*array++);
+}
