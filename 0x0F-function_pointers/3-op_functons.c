5a31536652a685a60a9b63af453da32874164cac
amrabdelghany <joo_amr91@yahoo.com>
2023-09-15 17:14:21 -0700

    done

---
+++ 0x0F-function_pointers/3-op_functons.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - func return sum of a and b
+ * @a: int input
+ * @b: int input
+ * Return: a + b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - func returns sub a and b
+ * @a: input int
+ * @b: input int
+ * Return: a - b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - func returns multiplication of a and b
+ * @a: input int
+ * @b: input int
+ * Return: a * b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - func returns division of a and b
+ * @a: input int
+ * @b: input int
+ * Return: a / b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - func returns module a by b
+ * @a: input int
+ * @b: input int
+ * Return: a % b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
