086ed3647d120728f1c7b1afc4ee77d47a1cefe4
Amr Abdelghany <joo_amr91@yahoo.com>
2023-09-28 18:36:25 -0700

    done

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ * move_past_star - iter the past *
+ * @s2: str input
+ * Return: ptr
+*/
+
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * inception - makes magic reality
+ * @s1: first input
+ * @s2: 2nd input
+ * Return: 1 or 0
+*/
+
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wildcmp - compares two strings and returns 1 if the strings
+ *		can be considered identical, otherwise return 0
+ * @s1: pointer input
+ * @s2: pointer input
+ * Return: 1 or 0
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+	return (0);
+}
