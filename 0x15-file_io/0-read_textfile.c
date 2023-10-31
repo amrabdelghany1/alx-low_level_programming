ea576667d8a2fdac2e875bd8abaa23f6c2aca31c
Amr Abdelghany <joo_amr91@yahoo.com>
2023-10-30 20:09:18 -0700

    done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - it's read the text from a file and print it
+ * @filename: file input to read
+ * @letters: bytes input to read
+ *
+ * Return: read-printed bytes number
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fp = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &but[0], bytes);
+	close(fp);
+	return (bytes);
+}
