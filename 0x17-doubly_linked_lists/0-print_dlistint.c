ef0d53b5699c52009c24c9840b4e9c681add4dfb
Amr Abdelghany <joo_amr91@yahoo.com>
2023-12-09 19:01:10 -0800

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,10 +7,12 @@
  * Return: list size
 */
 
-size_t print_dlistint(const dlistint_t *h) {
+size_t print_dlistint(const dlistint_t *h)
+{
 	size_t i = 0;
 
-	while (h) {
+	while (h)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
